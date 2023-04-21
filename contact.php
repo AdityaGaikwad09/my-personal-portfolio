fdfb8a0fdefb733bababb9758aeb9cb7fe0b87d9
Aditya Gaikwad <122694503+AdityaGaikwad09@users.noreply.github.com>
2023-04-21 16:53:15 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,10 +1,16 @@
 <?php
 //get data from form  
-
 $name = $_POST['name'];
 $email= $_POST['email'];
 $number= $_POST['number'];
 $message= $_POST['message'];
+
+//sanitize and validate email
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Invalid email address";
+    exit;
+}
 
 $to = "adityatraid09@gmail.com";
 
@@ -14,15 +20,14 @@ $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n  Mobile number = " . 
 Message =" . $message;
 
 $headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com" . "\r\n" .
+"Reply-To: " . $email . "\r\n" .
+"X-Mailer: PHP/" . phpversion();
 
-"CC: somebodyelse@example.com";
-
-if($email!=NULL){
-
-    mail($to,$subject,$txt,$headers);
+//send email
+if(mail($to, $subject, $txt, $headers)){
+    header("Location: thankyou.html");
+} else {
+    echo "Error sending email";
 }
-
-//redirect
-header("Location:thankyou.html");
-
 ?>
